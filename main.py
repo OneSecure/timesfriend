1f81b5ecbde5501305231df4eca287fce16378ab
KeySecure <keysecuresale@gmail.com>
2016-03-02 15:55:05 +0800

    Weibo and Twitter post divided.

--- main.py
+++ main.py
@@ -11,16 +11,22 @@ from datetime import datetime
 
 from timesfriend import postWeibo
 from timesfriend import generateTweet
-     
-class DataRetriever(webapp2.RequestHandler):
+
+class PostTimeToWeibo(webapp2.RequestHandler):
 	def get(self):
 		info = generateTweet()
 		postWeibo(info)
 
+
+class PostTimeToTwitter(webapp2.RequestHandler):
+	def get(self):
+		info = generateTweet()
 		from tweetpost import postTweet
 		postTweet(info)
 
+
 APP = webapp2.WSGIApplication(
-        [
-            ('/retrievedata', DataRetriever)
-        ])
+		[
+			('/posttimetoweibo', PostTimeToWeibo),
+			('/posttimetotwitter', PostTimeToTwitter),
+		])
